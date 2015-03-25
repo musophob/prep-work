fa91ae9e8475d24fb3633b9f3da3a96f9c671b60
Sean Henderson <mail@sean.sh>
2015-03-25 17:06:10 +0200

    12 passing

--- coding-test-1/practice-problems/problems/12-dasherize-number.rb
+++ coding-test-1/practice-problems/problems/12-dasherize-number.rb
@@ -8,6 +8,43 @@
 # Difficulty: medium.
 
 def dasherize_number(num)
+
+  # create an array with each digit in a separate index
+  nums = num.to_s.split("")
+
+  # loop through each digit; if it's odd, add dashes to both sides
+  i = 0
+  while i < nums.length
+    if nums[i].to_i % 2 != 0
+      nums[i] = "-" + nums[i] + "-"
+    end
+    i += 1
+  end
+
+  # smash the array back into a string, with the shiny new dashes
+  nums = nums.join.split("")
+
+  # check for any nefarious dashes at the beginning/end of the string and 
+  # make them go away
+  if nums[0] == "-"
+    nums.shift
+  end
+  if
+    nums[-1] == "-"
+    nums.pop
+  end
+
+  # in the event of a double dash, make it a single dash
+  i = 0
+  while i < nums.length
+    if nums[i] == "-" && nums[i+1] == "-"
+      nums[i] = ""
+    end
+    i += 1
+  end
+
+  # return an inconspicuous string; go make more coffee
+  return nums.join
 end
 
 # These are tests to check that your code is working. After writing
