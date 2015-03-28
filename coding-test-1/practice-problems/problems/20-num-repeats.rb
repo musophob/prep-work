9ead1ac4e05124cd474aed35b3b686ed738abb33
Sean Henderson <mail@sean.sh>
2015-03-28 22:01:05 +0200

    1-20 passing

--- coding-test-1/practice-problems/problems/20-num-repeats.rb
+++ coding-test-1/practice-problems/problems/20-num-repeats.rb
@@ -7,8 +7,29 @@
 # Difficulty: hard.
 
 def num_repeats(string)
+  letters = string.split("").sort
+  idx = 0
+  repeats = 0
+  while idx < letters.length
+    repeat = false
+    idx2 = idx + 1
+    last_repeat = 0
+    while idx2 < letters.length
+      if letters[idx2] == letters[idx]
+        repeat = true
+        last_repeat = idx2
+      end
+      idx2 += 1
+    end
+    if repeat == true
+      repeats += 1
+      idx = last_repeat + 1
+    else
+      idx += 1
+    end
+  end
+  return repeats
 end
-
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
 
